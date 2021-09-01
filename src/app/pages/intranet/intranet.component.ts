e7a4a28f83433dc8bc270fff7da9b499858859f3
chemo37 <88467995+chemo37@users.noreply.github.com>
2021-09-01 18:36:55 -0500

    Add files via upload

---
+++ src/app/pages/intranet/intranet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-intranet',
+  templateUrl: './intranet.component.html',
+  styleUrls: ['./intranet.component.css']
+})
+export class IntranetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
